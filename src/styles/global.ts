2ae6943cc214c192e08647f33ab96f1a9b628347
Gabriel Pereira Bastos <gabriel.bastos@devmagic.com.br>
2022-04-01 12:46:15 -0300

    add jest-styled-components dependency

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -11,7 +11,11 @@ const GlobalStyles = createGlobalStyle`
     font-size: 62.5%;
   }
 
-  body{
+  html, body, #__next {
+    height: 100%;
+  }
+  
+  body {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
     Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
